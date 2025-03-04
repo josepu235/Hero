f849b5c0f512fecf970f1314121541a8a5a7582d
josepu235 <sogradiel123@gmail.com>
2025-03-04 23:40:28 +0800

    Refactor home to responsive

--- src/page/Hero.jsx
+++ src/page/Hero.jsx
@@ -5,30 +5,33 @@ import './Hero.css'
 
 function Hero() {
 
-  return (
-    
-    <div>
-      <nav id="desktop-nav">
-      <div>
-        <ul class="nav-links">
-        <li><Link to="/about-me">About Me</Link></li>
-          <li><Link to="#projects">Projects</Link></li>
-          <li><Link to="#contact">Contact</Link></li>
+  return (<>
+    <div className='hero-wrapper bg-main'>
+      <nav className='nav'>
+        <ul className='nav__list'>
+          <li className='nav__item'>
+            <Link to={'/about-me'} className='nav__link'>About me</Link>
+          </li>
+          <li className='nav__item'>
+            <Link to={'#'} className='nav__link'>Projects</Link>
+          </li>
+          <li className='nav__item'>
+            <Link to={'#'} className='nav__link'>Contact</Link>
+          </li>
         </ul>
-      </div>
-    </nav>
-      <section className="Hero-prof">
-        <div className="Hero-pic">
+      </nav>
+
+      <div className='hero'>
+        <div className="hero__info">
+          <h1 className="Hero-intro typing">Hello, I'm</h1>
+          <h2 className="Hero-name typing delay-1">Josef Nathan Sogradiel</h2>
+          <h3 className='title typing delay-2'>A Student and a Developer</h3>
+        </div>
+        <div className='hero__img-wrapper'>
           <img src={pp} alt="Josef profile picture" />
         </div>
-        <div className="Hero-text">
-          <h2 className="Hero-intro typing">Hello, I'm</h2>
-          <h1 className="Hero-name typing delay-1">Josef Nathan Sogradiel</h1>
-          <h1 className='title typing delay-2'>A Student and a Developer</h1>
-
-        </div>
-      </section>
+      </div>
     </div>
-  );
+  </>);
 }
 export default Hero;
